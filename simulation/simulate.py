7c87b1df389f25d530f11d45d9261f578a0d3a05
dawnkd <dawnkd@rpi.edu>
2019-04-18 07:34:44 -0400

    lots of plots

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -2,13 +2,24 @@
 import numpy as np
 from numpy import linalg as la
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib
 import math
 import itertools
+from sympy import *
 from sympy.solvers.solveset import linsolve
 
 from algorithm import *
 
-def plot(data, shift, f0, sample_period):
+SAMPLE_FREQUENCY = 44100
+N = 8192
+F0 = 21000
+P = np.array([0,0])
+VELOCITY = np.array([1,1])
+SENSORS = [np.array([3,5]), np.array([5,1]), np.array([1,0.5])]
+
+
+def plot(data, shift, f0, sample_period, s, fpb):
 	# print(f0, shift)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
@@ -19,64 +30,191 @@ def plot(data, shift, f0, sample_period):
 
 	peak_range = doppler(f0, max_vel)
 
+	# find index of shift peak
 	f0_bin = round(f0 / fpb)
 	bin_range = int(peak_range / fpb)
-
-	max_peak = find_next_peak(data)
-	min_peak = f0_bin if max_peak != f0_bin else None
 
 	left_peak = f0_bin - find_next_peak(data[f0_bin:f0_bin-bin_range:-1])
 	right_peak = f0_bin + find_next_peak(data[f0_bin:f0_bin+bin_range])
 	max_peak = max((left_peak, right_peak),
 				key=lambda x: data[x] if 0 <= x < len(data) else 0)
-	# print("peaks", max_peak, f0_bin)
+	# get range to include in graph
 	peak_start = find_next_low(data[max_peak:max_peak-bin_range:-1])
 	peak_end = find_next_low(data[max_peak:max_peak+bin_range])
 	peak_start = peak_end = min(peak_start, peak_end) + 5
+	# assign peaks to left and right
 	(left_peak, right_peak) = (max_peak, f0_bin) if max_peak < f0_bin else (f0_bin, max_peak)
 
 	plottable = data[left_peak-peak_start:right_peak+peak_end]
-	# xdata = [i*(f0/len(data)) for i in range(len(plottable))]
-	# xdata = [i for i in range(len(plottable))]
 
-
-	# plottable = data[left_peak-peak_start:right_peak]
-	# print(len(plottable), len(data))
-	# # xdata = [(i+left_peak-peak_start) for i in range(len(plottable))]
 	xdata = np.array(range(left_peak-peak_start, left_peak-peak_start+len(plottable)))*f0/len(data)
 
 	line, = ax.plot(xdata, plottable)
 
 	max_y = max(plottable)
-	xpos = data.index(max_y)*(f0/len(data))
-	print("peaks", max_peak, f0_bin)
-	f0_pos = f0_bin*f0/len(data)
-	shift_pos = max_peak*f0/len(data)
+	xpos = data.index(max_y)*fpb
+	# print("peaks", max_peak, f0_bin)
+	f0_pos = f0_bin*fpb
+	shift_pos = max_peak*fpb
 	ax.annotate('f_0', xy=(f0_pos, data[f0_bin]), xytext=(f0_pos, max_y+5), ha="center")
 	ax.annotate('shift', xy=(shift_pos, data[max_peak]), xytext=(shift_pos, max_y+5), ha="center")
 	# ax.text(xpos, max_y, "local max", ha="center")
-
+	fig.suptitle("Sensor at ({},{})".format(*s))
 	fig.show()
 
-def plot_2d_layout(sensors, p, v):
-	fig = plt.figure()
+def plot_all_spectrums(spec, f0, peak_indices, x_min, x_max, fpb):
+	fig = plt.figure("spectrum")
 	ax = fig.add_subplot(111)
 
+	xdata = np.array(range(x_min, x_max))*fpb
+	lines = []
+	offset = max(spec[0])*0.01
+	for i,s in enumerate(spec):
+		line, = ax.plot(xdata,s[x_min:x_max], label="s{}".format(i))
+		lines.append(line)
+	ax.annotate('f0', xy=(f0, max(spec[0])), xytext=(f0, max(spec[0])+offset), ha="center")
+	for i,x in enumerate(peak_indices):
+		ax.annotate('s{}'.format(i), xy=(x*fpb, spec[i][x]), xytext=(x*fpb, spec[i][x]+offset), ha='center')
+	ax.legend()
+	fig.suptitle("Frequency Shift due to the Doppler Effect")
+	plt.xlabel("Frequncy (Hz)")
+	plt.ylabel("Intensity")
+	fig.show()
+
+def find_plot_chars(spectrum, f0, sample_period):
+	xmin = 999999
+	xmax = 0
+	peaks = []
+	for s in spectrum:
+		samples = (len(s)-1)*2
+		fpb = freq_per_bin(sample_period, samples)
+
+		peak_range = doppler(f0, max_vel)
+
+		# find index of shift peak
+		f0_bin = round(f0 / fpb)
+		bin_range = int(peak_range / fpb)
+
+		left_peak = f0_bin - find_next_peak(s[f0_bin:f0_bin-bin_range:-1])
+		right_peak = f0_bin + find_next_peak(s[f0_bin:f0_bin+bin_range])
+		max_peak = max((left_peak, right_peak),
+					key=lambda x: s[x] if 0 <= x < len(s) else 0)
+		# get range to include in graph
+		peak_start = find_next_low(s[max_peak:max_peak-bin_range:-1])
+		peak_end = find_next_low(s[max_peak:max_peak+bin_range])
+		peak_start = peak_end = min(peak_start, peak_end) + 5
+		(left_peak, right_peak) = (max_peak, f0_bin) if max_peak < f0_bin else (f0_bin, max_peak)
+
+		if left_peak - peak_start < xmin: xmin = left_peak - peak_start
+		if right_peak + peak_start > xmax: xmax = right_peak + peak_start
+		peaks.append(max_peak)
+	# peaks.append(f0_bin)
+
+	return xmin,xmax,peaks,fpb
+
+def on_press(event):
+    global SAMPLE_FREQUENCY, N, F0, P, VELOCITY, SENSORS
+    print("====================================")
+    print("====================================")
+    print('Rerunning simulation using P=({},{})'.format(event.xdata, event.ydata))
+    print("====================================")
+    print("====================================")
+    P = np.array([event.xdata,event.ydata])
+    plt.close("2d")
+    plt.close("spectrum")
+    simulate_multiple_sensors(SAMPLE_FREQUENCY, N, F0, P, VELOCITY, SENSORS)
+
+def move_figure(f, x, y):
+    """Move figure's upper left corner to pixel (x, y)"""
+    backend = matplotlib.get_backend()
+    if backend == 'TkAgg':
+        f.canvas.manager.window.wm_geometry("+%d+%d" % (x, y))
+    elif backend == 'WXAgg':
+        f.canvas.manager.window.SetPosition((x, y))
+    else:
+        # This works for QT and GTK
+        # You can also use window.setGeometry
+        f.canvas.manager.window.move(x, y)
+
+def plot_2d_layout(sensors, p, v, v_rads, dopple_vs, sol):
+	fig = plt.figure("2d")
+	ax = fig.add_subplot(111)
+
+	xmin, xmax = ax.get_xlim()
+	ymin, ymax = ax.get_ylim()
+	xmin = -.25
+	ymin = -.25
+
+	# print(ax.get_xlim())
+	# print(ax.get_ylim())
 	data = [*sensors,p]
-	vel = [*p,*v]
 	px, py = zip(*data)
-	vx, vy, vu, vv = zip(vel)
 	y_range = max(py) - min(py)
-	vel_text = p-v*0.08
-	print("data",data)
+	vel_text = p-v*0.2
+
+	vx, vy, vu, vv = [*p,*v]
+	ax.quiver(vx, vy, vu, vv, angles='xy', scale_units='xy', scale=1, color='g', alpha=0.5)
+	xmin = vx-vu - 0.25 if vx-vu - 0.25 < xmin else xmin
+	xmax = vx+vu + 0.25 if vx+vu + 0.25 > xmax else xmax
+	ymin = vy-vv - 0.25 if vy-vv - 0.25 < ymin else ymin
+	ymax = vy+vv + 0.25 if vy+vv + 0.25 > ymax else ymax
+
+	if sol is not None:
+		vx, vy, vu, vv = [*p,*sol]
+		ax.quiver(vx, vy, vu, vv, angles='xy', scale_units='xy', scale=1, color='r', alpha=0.5)
+		xmin = vx-vu - 0.25 if vx-vu - 0.25 < xmin else xmin
+		xmax = vx+vu + 0.25 if vx+vu + 0.25 > xmax else xmax
+		ymin = vy-vv - 0.25 if vy-vv - 0.25 < ymin else ymin
+		ymax = vy+vv + 0.25 if vy+vv + 0.25 > ymax else ymax
+
+	
+	text_pos = []
+	for s,v in zip(sensors,v_rads):
+		proj = np.dot(v,(p-s)/la.norm(p-s))
+		vx,vy,vu,vv = [*s,*proj]
+		ax.quiver(vx, vy, vu, vv, angles='xy', scale_units='xy', scale=1, color='g', alpha=0.5)
+
+		xmin = vx-vu - 0.25 if vx-vu - 0.25 < xmin else xmin
+		xmax = vx+vu + 0.25 if vx+vu + 0.25 > xmax else xmax
+		ymin = vy-vv - 0.25 if vy-vv - 0.25 < ymin else ymin
+		ymax = vy+vv + 0.25 if vy+vv + 0.25 > ymax else ymax
+
+		text_pos.append(s-proj*0.2)
+
+	for s,v in zip(sensors,dopple_vs):
+		proj = np.dot(v,(p-s)/la.norm(p-s))
+		vx,vy,vu,vv = [*s,*proj]
+		ax.quiver(vx, vy, vu, vv, angles='xy', scale_units='xy', scale=1, color='r', alpha=0.5)
+
+		xmin = vx-vu - 0.25 if vx-vu - 0.25 < xmin else xmin
+		xmax = vx+vu + 0.25 if vx+vu + 0.25 > xmax else xmax
+		ymin = vy-vv - 0.25 if vy-vv - 0.25 < ymin else ymin
+		ymax = vy+vv + 0.25 if vy+vv + 0.25 > ymax else ymax
+
+
 	ax.scatter(px,py)
 	for i,(x,y) in enumerate(data):
 		m = "s{}".format(i) if i < len(sensors) else "P"
-		ax.annotate(m, xy=(x,y), xytext=(x,y+y_range*0.02) if i < len(sensors) else (vel_text[0], vel_text[1]), ha="center")
-	ax.quiver(vx, vy, vu, vv, angles='xy', scale_units='xy', scale=1)
+		ax.annotate(m, xy=(x,y), xytext=(tuple(text_pos[i])) if i < len(sensors) else tuple(vel_text), ha="center")
+	
+	red_patch = mpatches.Patch(color='red', label='Simulated')
+	green_patch = mpatches.Patch(color='green', label='Ideal')
+	plt.legend(handles=[red_patch, green_patch])
+
+	cid = fig.canvas.mpl_connect('button_press_event', on_press)
+
+	if ymin > -0.25: ymin = -0.25
+	if xmin > -0.25: xmin = -0.25
+	plt.xlim(xmin,xmax)
+	plt.ylim(ymin,ymax)
+	fig.suptitle("Simulated vs. Ideal Radial Velocities")
+	
+	move_figure(fig, 1000, 100)
+
 	fig.show()
 
 def simulate_single_sensor(sample_freq, n, f0, p, v):
+	velocity_vector = v
 	print("Simulation")
 	print("----------")
 	period = 1/sample_freq
@@ -100,7 +238,7 @@ def simulate_single_sensor(sample_freq, n, f0, p, v):
 	signal = list(signal_gen(period, n, np.array([0, 0]), p, v, f0))
 	spec = freq_spectrum(signal)
 	print("Frequency Bins: ", len(spec))
-	u, sigma = find_shift(period, f0, spec)
+	u, sigma, attenuation = find_shift(period, f0, spec)
 	print("Frequency Shift: ", u, " (", sigma, ")")
 	dopple_v, dopple_sigma = inv_doppler(f0, u), inv_doppler(f0, sigma)
 	print("Doppler Velocity: ", dopple_v, " (", dopple_sigma, ")")
@@ -111,7 +249,14 @@ def simulate_single_sensor(sample_freq, n, f0, p, v):
 	plot(spec, u, f0, period)
 
 def simulate_multiple_sensors(sample_freq, n, f0, p, v, sensors):
-    plot_2d_layout(sensors, p, v)
+    global SAMPLE_FREQUENCY, N, F0, P, VELOCITY, SENSORS
+    SAMPLE_FREQUENCY = sample_freq
+    N = n
+    F0 = f0
+    P = p
+    VELOCITY = v
+    SENSORS = sensors
+    velocity_vector = v
     print("Simulation")
     print("----------")
     period = 1/sample_freq
@@ -124,6 +269,10 @@ def simulate_multiple_sensors(sample_freq, n, f0, p, v, sensors):
     print("Velocity vector: ", v)
     v_rads = [np.dot(v, (p - s) / la.norm(p-s) ) for s in sensors]
 
+    print("Expexted distance from P: ", end='')
+    for i,s in enumerate(sensors):
+    	print("s{}: {}".format(i, la.norm(p-s)), end=', ' if i < len(sensors)-1 else '\n')
+
     print("Expected radial velocity: ", end='')
     for i,vr in enumerate(v_rads):
         print("s{}: {}".format(i, vr), end=', ' if i < len(v_rads)-1 else '\n')
@@ -133,20 +282,28 @@ def simulate_multiple_sensors(sample_freq, n, f0, p, v, sensors):
     print("Data Bounds: ", *list(resolution(period, n, f0, vr) for vr in v_rads), " bins")
     print("Time per FFT: ", period * n)
     print("Distance per FFT: ", period * n * la.norm(v))
+
+    
+
     print("------------------------------")
     print("Simulating")
     print("----------")
-    print(*sensors)
+    # print(*sensors)
     signal = [list(signal_gen(period, n, s, p, v, f0)) for s in sensors]
     spec = [freq_spectrum(sig) for sig in signal]
     print("Frequency Bins: ", len(spec[0]))
-    shift_sigma = [find_shift(period, f0, sp) for sp in spec]
+    shift_sigma_att = [find_shift(period, f0, sp) for sp in spec]
 
     print("Frequency Shift: ", end='')
-    for i,sh in enumerate(shift_sigma):
-        print("s{}: {} ({})".format(i, *sh), end=', ' if i < len(shift_sigma)-1 else '\n')
+    for i,sh in enumerate(shift_sigma_att):
+        print("s{}: {} ({})".format(i, *sh), end=', ' if i < len(shift_sigma_att)-1 else '\n')
 
-    dopplev_sigma = [(inv_doppler(f0, u), inv_doppler(f0, sigma)) for u,sigma in shift_sigma]
+    dopplev_sigma = [(inv_doppler(f0, u), inv_doppler(f0, sigma)) for u,sigma,_ in shift_sigma_att]
+
+    attenuation = [i[2] for i in shift_sigma_att]
+    print("Distance from P: ", end='')
+    for i,a in enumerate(attenuation):
+    	print("s{}: {}".format(i, 1/a), end=', ' if i < len(attenuation)-1 else '\n')
 
     print("Doppler Velocity: ", end='')
     for i,d in enumerate(dopplev_sigma):
@@ -160,9 +317,10 @@ def simulate_multiple_sensors(sample_freq, n, f0, p, v, sensors):
 
     print("----------------------")
     print("Plotting")
-    for i,s in enumerate(spec):
-        plot(s, shift_sigma[i][0], f0, period)
-
+    x_min, x_max, peak_indices, fpb = find_plot_chars(spec, f0, period)
+    # for i,s in enumerate(spec):
+    #     plot(s, shift_sigma_att[i][0], f0, period, sensors[i], fbp)
+    plot_all_spectrums(spec, f0, peak_indices, x_min, x_max, fpb)
     print("----------------------")
     print("Veloceration")
     print("----------------------")
@@ -170,11 +328,18 @@ def simulate_multiple_sensors(sample_freq, n, f0, p, v, sensors):
     dopple_vs = [v[0] for v in dopplev_sigma]
     A, b, v, r = veloceration_eqs(sensors,dopple_vs)
 
-    # print((*v,*r))
+    # print({r:1/a for r,a in zip(r, attenuation)})
+    A = [e.subs({r:1/a for r,a in zip(r, attenuation)}) for e in A]   
+
+    # print(linsolve(A,v))
     # print(solve(A))
-    # print(la.solve(A,b))
-    # A = np.array([])
-    # # b = np.array([r**2 - radii[-1]**2 - s[0]**2 - s[1]**2 + sensors[-1][0]**2 + sensors[-1][1]**2 for r,s in radii,sensors])
-    # print(A,b)
-    # x = la.solve(A,b)
-    # print(x)
+
+    sol = linsolve(A,v)
+    if sol:
+        sol = np.array(sol.args[0], dtype=float)
+        print("Velocity Vector: [{},{}]".format(*sol))
+    else:
+        print("No solution found")
+        sol = None
+    # print(sol)
+    plot_2d_layout(sensors, p, velocity_vector, v_rads, dopple_vs, sol)
