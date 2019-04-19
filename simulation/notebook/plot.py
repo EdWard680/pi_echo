bcb970a073858467d67544334544015d73f12132
dawnkd <dawnkd@rpi.edu>
2019-04-19 07:10:47 -0400

    p is q

--- simulation/notebook/plot.py
+++ simulation/notebook/plot.py
@@ -20,7 +20,6 @@ def side_by_side(title=None):
     return ax1,ax2
 
 def not_side_by_side(title=None):
-    fig=plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
     fig = plt.figure()
     ax = fig.add_subplot(111)
     if title is not None: fig.suptitle(title)
@@ -64,7 +63,7 @@ def maintain_bounds(xmin,xmax,ymin,ymax,x,y,u,v):
     ymax = y+v + 0.25 if y+v + 0.25 > ymax else ymax
     return xmin,xmax,ymin,ymax
 
-def plot_layout(ax, sensors, p=None, v=None, vr=None, vd=None, sv=None, sp=None, xlabel=None, ylabel=None, title=None):
+def plot_layout(ax, sensors, q=None, v=None, vr=None, vd=None, sq=None, sv=None, xlabel=None, ylabel=None, title=None):
     xmin, xmax = ax.get_xlim()
     ymin, ymax = ax.get_ylim()
     offset = 0.2
@@ -74,38 +73,81 @@ def plot_layout(ax, sensors, p=None, v=None, vr=None, vd=None, sv=None, sp=None,
     if vr is None:
         for i,(x,y) in enumerate(sensors):
             ax.annotate("S{}".format(i), xy=(x,y), xytext=(x, y+offset), ha='center')
+            xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,x,y,0,offset)
 
-    if p is not None:
-        px,py = p[0], p[1]
+    if q is not None:
+        px,py = q[0], q[1]
         ax.scatter(px,py)
         if v is None:
-            ax.annotate("P", xy=(px,py), xytext=(px, py+offset), ha='center')
+            ax.annotate("Q", xy=(px,py), xytext=(px, py+offset), ha='center')
+            xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,x,y,0,offset)
 
-    if v is not None and p is not None:
-        vx,vy,vu,vv = [*p,*v]
+    if v is not None and q is not None:
+        vx,vy,vu,vv = [*q,*v]
         ax.quiver(vx,vy,vu,vv, angles='xy', scale_units='xy', scale=1, color='g', alpha=0.5)
         xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,vx,vy,vu,vv)
 
-        pos = p - (v)/la.norm(v)*offset
-        ax.annotate("P", xy=tuple(p), xytext=tuple(pos), ha='center', , va='center')
+        pos = q - (v)/la.norm(v)*offset
+        ax.annotate("Q", xy=tuple(q), xytext=tuple(pos), ha='center', va='center')
+        xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,q[0],q[1],pos[0]-q[0],pos[1]-q[1])
 
         green_patch = mpatches.Patch(color='green', label='Ideal', alpha=0.5)
         ax.legend(handles=[green_patch], loc='upper left')
 
     if vr is not None:
         for i,(s,v_rad) in enumerate(zip(sensors,vr)):
-            r = (p-s)/la.norm(p-s)
-            proj = np.dot(r,v)*r
+            if v_rad > 0:
+                r = (q-s)/la.norm(q-s)
+                proj = np.dot(r,v)*r
+                vx,vy,vu,vv = [*s,*proj]
+                pos = s - proj/la.norm(proj) * offset
+            else:
+                r = (q-s)/la.norm(q-s)
+                proj = np.dot(r,v)*r
+                start = s - proj
+                vx,vy,vu,vv = [*start,*proj]
+                pos = s + proj/la.norm(proj) * offset
             # print(proj)
-            vx,vy,vu,vv = [*s,*proj]
-            ax.quiver(vx,vy,vu,vv, angles='xy', scale_units='xy', scale=1, color='g', alpha=0.5)
+            
+            ax.quiver(vx,vy,vu,vv, angles='xy', scale_units='xy', scale=1, color='g', alpha=0.5, width=0.011)
             xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,vx,vy,vu,vv)
 
-            pos = s - proj/la.norm(proj) * offset
+            
             ax.annotate("S{}".format(i), xy=tuple(s), xytext=tuple(pos), ha='center', va='center')
+            xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,s[0],s[1],pos[0]-s[0],pos[1]-s[1])
 
         green_patch = mpatches.Patch(color='green', label='Ideal', alpha=0.5)
         ax.legend(handles=[green_patch], loc='upper left')
+
+    if vd is not None:
+        for i,(s,v_dop) in enumerate(zip(sensors,vd)):
+            if v_dop > 0:
+                r = (q-s)/la.norm(q-s)
+                proj = np.dot(r,v)*r
+                vx,vy,vu,vv = [*s,*proj]
+                pos = s - proj/la.norm(proj) * offset
+            else:
+                r = (q-s)/la.norm(q-s)
+                proj = np.dot(r,v)*r
+                start = s - proj
+                vx,vy,vu,vv = [*start,*proj]
+                pos = s + proj/la.norm(proj) * offset
+            # print(proj)
+            
+            ax.quiver(vx,vy,vu,vv, angles='xy', scale_units='xy', scale=1, color='r', alpha=0.5)
+            xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,vx,vy,vu,vv)
+
+        red_patch = mpatches.Patch(color='red', label='Simulated', alpha=0.5)
+        ax.legend(handles=[green_patch, red_patch], loc='upper left')
+
+    if sq is not None: 
+        px,py = sq[0], sq[1]
+        ax.scatter(px,py)
+        if sv is None:
+            ax.annotate("Q^", xy=(px,py), xytext=(px, py+offset), ha='center')
+            xmin,xmax,ymin,ymax = maintain_bounds(xmin,xmax,ymin,ymax,x,y,0,offset)
+    else:
+        sq = v
 
     ax.set_xlim(xmin,xmax)
     ax.set_ylim(ymin,ymax)
